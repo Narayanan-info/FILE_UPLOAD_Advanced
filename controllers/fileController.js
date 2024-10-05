515fe395549768b7f3de94e89e636161d115c87b
Narayanan-info <narayanan.k.info@gmail.com>
2024-10-05 21:00:02 +0530

    added jpeg & png sanitized

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -2,6 +2,7 @@ const { saveFileLocally } = require('../services/fileService');
 const { renameFile, sanitizeFilename, computeFileChecksum } = require('../utils/checksum');
 const xss = require('xss');
 const fs = require('fs');
+const sharp = require('sharp')
 
 exports.uploadFile = async (req, res) => {
   if (!req.file) {
@@ -12,6 +13,21 @@ exports.uploadFile = async (req, res) => {
     const validFileTypes = ['image/jpeg', 'image/png', 'application/pdf', 'image/svg+xml'];
     if (!validFileTypes.includes(req.file.mimetype)) {
       return res.status(400).json({ error: 'Invalid file type. Only JPEG, PNG, PDF, and SVG are allowed.' });
+    }
+
+    if (req.file.mimetype === 'image/jpeg, image/png') {
+      try{
+        const sanitizedImagePath = `sanitized-${req.file.filename}`;
+        await sharp(req.file.path)
+          .withMetadata({ exif: false })
+          .toFile(sanitizedImagePath);
+
+        fs.unlinkSync(req.file.path); 
+        fs.renameSync(sanitizedImagePath, req.file.path);
+
+      } catch (err) {
+        return res.status(500).json({ error: 'Error processing JPEG file' });
+      }
     }
 
     if (req.file.mimetype === 'image/svg+xml') {
